4ce5ba8a35e39294484b4ce2f867efa75605168c
jh318 <jhawley318@gmail.com>
2017-02-25 02:56:46 -0600

    not sure

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    static int scoreP1 = 0;
+    static int scoreP2 = 0;
+
+    private void Start()
+    {
+        
+    }
+
+    public static void UpdateP1Score()
+    {
+        scoreP1 += 1;
+        Debug.Log("Player 1 score: " + scoreP1);
+    }
+
+    public static void UpdateP2Score()
+    {
+        scoreP2 += 1;
+        Debug.Log("Player 2 score: " + scoreP2);
+    }
+
+}
--- Assets/Scripts/P1CatchScript.cs
+++ Assets/Scripts/P1CatchScript.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CatchScript : MonoBehaviour {
+public class P1CatchScript : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D c)
     {
         if(c.gameObject.tag == "Cat")
         {
             Debug.Log("Caught The Cat");
+
+            GameManager.UpdateP1Score();
             
             c.gameObject.SetActive(false);
         }
