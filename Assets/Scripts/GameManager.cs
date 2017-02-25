b735e54816e404c221e7229a5f5301d95e3a9b65
CHRIS\Chris Matthews <cmatthews@dmi-intertech.org>
2017-02-25 14:19:51 -0600

    added UI elements

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,27 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
-    static int scoreP1 = 0;
-    static int scoreP2 = 0;
+
+    public static GameManager instance;
+
+    int scoreP1 = 0;
+    int scoreP2 = 0;
+    
+    public Text p1ScoreUI;
+    public Text p2ScoreUI;
+
+    private void Awake()
+    {
+        instance.p1ScoreUI.text = "Player 1 score: " + instance.scoreP1;
+        instance.p2ScoreUI.text = "Player 2 score: " + instance.scoreP2;
+    }
 
     private void Start()
     {
         
+
     }
 
     public static void UpdateP1Score()
     {
-        scoreP1 += 1;
-        Debug.Log("Player 1 score: " + scoreP1);
+        instance.scoreP1 += 1;
+        Debug.Log("Player 1 score: " + instance.scoreP1);
+        instance.p1ScoreUI.text = "Player 1 score: " + instance.scoreP1;
     }
 
     public static void UpdateP2Score()
     {
-        scoreP2 += 1;
-        Debug.Log("Player 2 score: " + scoreP2);
+        instance.scoreP2 += 1;
+        Debug.Log("Player 2 score: " + instance.scoreP2);
+        instance.p2ScoreUI.text = "Player 2 score: " + instance.scoreP2;
     }
 
 }
