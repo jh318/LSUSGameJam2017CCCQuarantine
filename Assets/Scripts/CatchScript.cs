b2a652e00dd84c9bee8f9d1769693528222cdf5f
CHRIS\Chris Matthews <cmatthews@dmi-intertech.org>
2017-02-25 00:30:55 -0600

    catcher is working

---
+++ Assets/Scripts/CatchScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchScript : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if(c.gameObject.tag == "Cat")
+        {
+            Debug.Log("Tried To Catch a cat");
+            
+            c.gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,43 @@ public class PlayerController : MonoBehaviour {
 
 	
     public float speed;
+    public Transform visibleBody;
+    public GameObject Catcher;
+    public GameObject Cat;
 	
+
+    IEnumerator CatcherWait()
+    {
+
+        Debug.Log("User tried to catch something");               
+           
+            
+        Catcher.SetActive(true);
+        //if (Catcher.gameObject == Cat)
+        //{
+        //    Cat.SetActive(false);
+        //    Debug.Log("Caught The Cat");
+        //}
+                    
+        yield return new WaitForSeconds(.1f);
+
+        Catcher.SetActive(false);
+
+        
+
+    }
 
 	void Update(){
         float moveHorizontal = Input.GetAxis("Horizontal") * speed;
         float moveVert = Input.GetAxis("Vertical") * speed;
 
-        moveHorizontal *= Time.deltaTime;
-        moveVert *= Time.deltaTime;
-        transform.position += new Vector3(moveHorizontal, moveVert, 0);
+        Vector3 heading = new Vector3(moveHorizontal, moveVert, 0);
+        transform.position += heading * Time.deltaTime;
+        if (heading.sqrMagnitude > 0.1f) visibleBody.up = heading;
 		
+        if (Input.GetButtonDown("Catch"))
+        {
+            StartCoroutine("CatcherWait");
+        }
 	}
 }
